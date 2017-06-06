6058872b6de6601b904677b6f35c95360091aa93
ReltivlyObjectv <me@relativelyobjective.tech>
2017-06-06 01:08:53 -0700

    Added box physics and fixed Segmentation Fault.

--- main.cpp
+++ main.cpp
@@ -52,24 +52,29 @@ class Game {
 	private: 
 		static Shape paddle;
 		static std::list<Particle*> balls;
+		static std::list<Shape*> allBoxes;
 	public:
 		static void initialize();
 		static int getBallCount();
 		static Shape* getPaddle();
 		static std::list<Particle*>::iterator getBallIterator();
+		static std::list<Particle*>::iterator getLastBallIterator();
+		static std::list<Shape*>::iterator getShapesIterator();
+		static std::list<Shape*>::iterator getLastShapesIterator();
 		static void makeParticle();
 		static void removeParticle(Particle* p);
 };
 Shape Game::paddle;
 std::list<Particle*> Game::balls;
+std::list<Shape*> Game::allBoxes;
 
 void Game::initialize(){
 	//declare the paddle
-	Game::getPaddle()->width = PADDLE_WIDTH;
-	Game::getPaddle()->height = PADDLE_HEIGHT;
-	Game::getPaddle()->center.x = 120 + 5*65;
-	Game::getPaddle()->center.y = PADDLE_BUFFER + (0.5 * PADDLE_HEIGHT) ;
-	
+	paddle.width = PADDLE_WIDTH;
+	paddle.height = PADDLE_HEIGHT;
+	paddle.center.x = 120 + 5*65;
+	paddle.center.y = PADDLE_BUFFER + (0.5 * PADDLE_HEIGHT) ;
+	allBoxes.push_front(&paddle);
 }
 Shape* Game::getPaddle(){
 	return &paddle;
@@ -77,24 +82,35 @@ Shape* Game::getPaddle(){
 std::list<Particle*>::iterator Game::getBallIterator(){
 	return balls.begin();
 }
+std::list<Particle*>::iterator Game::getLastBallIterator(){
+	return balls.end();
+}
+std::list<Shape*>::iterator Game::getShapesIterator(){
+	return allBoxes.begin();
+}
+std::list<Shape*>::iterator Game::getLastShapesIterator(){
+	return allBoxes.end();
+}
 int Game::getBallCount(){
 	return balls.size();
 }
 void Game::makeParticle() {
 	if (getBallCount() >= MAX_PARTICLES){
+		std::cout << "Could not create particle" << std::endl;
 		return;
 	}
 	//position of particle
 	Particle *p = new Particle();
+	std::cout << "Creating particle " << p << std::endl;
 	p->s.center.x = Game::getPaddle()->center.x;
-	p->s.center.y = Game::getPaddle()->center.y;
+	p->s.center.y = Game::getPaddle()->center.y + (Game::getPaddle()->height / 2);
 	p->velocity.y = 4.0;
 	p->velocity.x = 1.0;
 	balls.push_front(p);
 }
 void Game::removeParticle(Particle* p){
+	std::cout << "Out of bounds - removing particle " << p << std::endl;
 	balls.remove(p);
-	std::cout << "Remove ball, there are now " << getBallCount() << "left.";
 }
 
 //Function prototypes
@@ -105,7 +121,7 @@ void check_mouse(XEvent *e);
 int check_keys(XEvent *e);
 void movement();
 void render();
-bool isInShape(Shape &shape, float x, float y, float z=0, bool square=true, bool inclusive=true);
+bool isInShape(Shape *shape, float x, float y, bool inclusive=false);
 void checkCollision();
 
 int main(void) {
@@ -232,14 +248,29 @@ int check_keys(XEvent *e) {
 void movement() {
 	if (Game::getBallCount() <= 0)
 		return;
-	std::list<Particle*>::iterator it = Game::getBallIterator();
-	for(int i = 0; i < Game::getBallCount(); i++, it++){
+	
+	for(std::list<Particle*>::iterator it = Game::getBallIterator(); it != Game::getLastBallIterator(); ){
 		Particle *p = *it;
 		p->s.center.x += p->velocity.x;
 		p->s.center.y += p->velocity.y;
 
 		//check for collision with shapes...
-		
+		for(std::list<Shape*>::iterator its = Game::getShapesIterator(); its != Game::getLastShapesIterator(); its++){
+			Shape *s = *its;
+			if(isInShape(s, p->s.center.x, p->s.center.y)){
+				//Hitting Shape
+				if(p->s.center.x < s->center.x){
+					//Hitting Bottom
+					p->velocity.y *= -1;
+				}else if(p->s.center.x > s->center.x){
+					//Hitting Top
+					p->velocity.y *= -1;
+				}else{
+					//Hitting Direct Middle
+					p->velocity.y *= -1;
+				}
+			}
+		}
 		//check for collision with edges...
 		if(p->s.center.x <= 0 || p->s.center.x >= WINDOW_WIDTH){
 			p->velocity.x *= -1;
@@ -249,10 +280,12 @@ void movement() {
 		}
 		//check for off-screen
 		if (p->s.center.y < 0.0) {
-			Game::removeParticle(*it);
+			Game::removeParticle(*(it++));
 			if(Game::getBallCount() < 1){
 				//TODO Lose
 			}
+		}else{
+			it++;
 		}
 	}
 }
@@ -298,46 +331,37 @@ void render() {
 
 
 
-bool isInShape(Shape &shape, float x, float y, float z, bool square, bool inclusive) {
-	if(square) {
-		//Square
-		float leftBound =  shape.center.x - (shape.width / 2);
-		float rightBound = shape.center.x + (shape.width / 2);
-		float upperBound = shape.center.y + (shape.height / 2);
-		float lowerBound = shape.center.y - (shape.height / 2);
-		if(inclusive) {
-			//Borders are considered part of the shape
-			if(x < leftBound) {
-				return false;
-			}else if(x > rightBound) {
-				return false;
-			}else if(y > upperBound) {
-				return false;
-			}else if(y < lowerBound) {
-				return false;
-			}else {
-				return true;
-			}
+bool isInShape(Shape *shape, float x, float y, bool inclusive) {
+	float leftBound =  shape->center.x - (shape->width);
+	float rightBound = shape->center.x + (shape->width);
+	float upperBound = shape->center.y + (shape->height);
+	float lowerBound = shape->center.y - (shape->height);
+	if(inclusive) {
+		//Borders are considered part of the shape
+		if(x < leftBound) {
+			return false;
+		}else if(x > rightBound) {
+			return false;
+		}else if(y > upperBound) {
+			return false;
+		}else if(y < lowerBound) {
+			return false;
 		}else {
-			//Borders are not considered part of the shape
-			if(x <= leftBound) {
-				return false;
-			}else if(x >= rightBound) {
-				return false;
-			}else if(y >= upperBound) {
-				return false;
-			}else if(y <= lowerBound) {
-				return false;
-			}else {
-				return true;
-			}
+			return true;
 		}
 	}else {
-		//Circle
-		//TODO
-		return false;
+		//Borders are not considered part of the shape
+		if(x <= leftBound) {
+			return false;
+		}else if(x >= rightBound) {
+			return false;
+		}else if(y >= upperBound) {
+			return false;
+		}else if(y <= lowerBound) {
+			return false;
+		}else {
+			return true;
+		}
 	}
-}
-void checkCollision(){
 
 }
